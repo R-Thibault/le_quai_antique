6c754098b22197cd575c96aa0d71dc09592f05e9
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2023-05-19 14:36:44 +0200

    fix planning crud

--- src/Controller/Admin/PlanningCrudController.php
+++ src/Controller/Admin/PlanningCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Planning;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class PlanningCrudController extends AbstractCrudController
 {
@@ -12,14 +17,24 @@ class PlanningCrudController extends AbstractCrudController
         return Planning::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            
+             TextField::new('day')->setLabel('Jour'),
+             TimeField::new('openAm')->setLabel('Ouverture midi'),
+              TimeField::new('closeAm')->setLabel('Fermeture midi'),
+              TimeField::new('openPm')->setLabel('Ouverture soir'),
+               TimeField::new('closePm')->setLabel('Fermeture soir'),
+            BooleanField::new('isClosedAm')->setLabel('Fermeture le midi')->setFormType(CheckboxType::class),
+            BooleanField::new('isClosedPm')->setLabel('Fermeture le soir')->setFormType(CheckboxType::class),
+                                                
+                                                        
+
+
+
         ];
     }
-    */
+    
 }
