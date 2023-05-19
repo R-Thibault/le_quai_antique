9760e68bc79757ad98e930dc2b1ac30fa9b53e86
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2023-05-19 11:25:54 +0200

    fix planning crud

--- src/Controller/Admin/PlanningCrudController.php
+++ src/Controller/Admin/PlanningCrudController.php
@@ -3,9 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Planning;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PlanningCrudController extends AbstractCrudController
@@ -15,26 +12,14 @@ class PlanningCrudController extends AbstractCrudController
         return Planning::class;
     }
 
-    
+    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            
-            TextField::new('day')->setLabel('Jour'),
-            NumberField::new('open_am')->setLabel('Ouverture midi'),
-            NumberField::new('close_am')->setLabel('Fermeture midi'),
-            NumberField::new('open_pm')->setLabel('Ouverture soir'),
-            NumberField::new('close_pm')->setLabel('Fermeture soir'),
-            
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
         ];
     }
-
-    public function configureCrud(Crud $crud): Crud
-    {
-        return $crud
-            ->setEntityLabelInSingular('Planning')
-            ->setEntityLabelInPlural('Plannings');
-            
-    }
-    
+    */
 }
