7c8db76d504190abfed7cdff312f2506e44f6616
R-Thibault <thibault.rossa@gmail.com>
2024-09-26 17:02:03 +0200

    package and code update

--- src/DataFixtures/ReservationsFixture.php
+++ src/DataFixtures/ReservationsFixture.php
@@ -14,34 +14,31 @@ class ReservationsFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
-        $users = $manager->getRepository(Users::class)->findBy( [], ['id' => 'ASC']);
+        $users = $manager->getRepository(Users::class)->findBy([], ['id' => 'ASC']);
+        
         for ($i = 0; $i < 30; $i++) {
             $reservation = new Reservations();
-    $reservation->setDateTime($faker->dateTimeBetween('now', '+5 days'));
-        
+            $reservation->setDateTime($faker->dateTimeBetween('now', '+5 days'));
             $reservation->setNbOfPersons($faker->numberBetween(1, 10));
-            $reservation->setLastname($faker->lastName);
-            $reservation->setFirstname($faker->firstName);
-            $reservation->setEmail($faker->email);
+            $reservation->setLastname($faker->lastName());  // Use method call
+            $reservation->setFirstname($faker->firstName());  // Use method call
+            $reservation->setEmail($faker->email());  // Use method call
             $reservation->setAmOrPm('pm');
             $manager->persist($reservation);
         }
-        for($i = 0; $i < 30; $i++){
-            
+        
+        for ($i = 0; $i < 30; $i++) {
             $reservation = new Reservations();
             $reservation->setDateTime($faker->dateTimeBetween('now', '+5 days'));
-            
             $reservation->setNbOfPersons($faker->numberBetween(1, 10));
             $reservation->setUser($faker->randomElement($users));
-            $reservation->setLastname($faker->lastName);
-            $reservation->setFirstname($faker->firstName);
-            $reservation->setEmail($faker->email);
+            $reservation->setLastname($faker->lastName());  // Use method call
+            $reservation->setFirstname($faker->firstName());  // Use method call
+            $reservation->setEmail($faker->email());  // Use method call
             $reservation->setAmOrPm('pm');
             $manager->persist($reservation);
         }
 
         $manager->flush();
     }
-
-    
 }
